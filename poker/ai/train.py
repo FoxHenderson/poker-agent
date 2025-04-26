dab23c5a4042d739701e9f0856133d9dcc9ec420
Fox Henderson <fox.t.henderson@durham.ac.uk>
2025-04-26 22:07:48 +0100

    basic cfr with regrets

--- poker/ai/train.py
+++ poker/ai/train.py
@@ -6,6 +6,7 @@ import time
 from copy import deepcopy
 from CFRState import CFR_State
 from cmdlogic.actions import Action
+from information_set import InformationSet, AbstractActions
 
 import sys
 
@@ -91,50 +92,8 @@ class CFR_Trainer:
 
             next_state = CFR_State(next_game, next_game.to_act_index)
             action_values[action] = self.cfr_recursive(next_game, next_state)
-
-    def cfr_recursive(self, game: Game, player: CFR_Player):
-        #print("RECURSION")
-        if game.is_hand_over():
-            t = game.get_terminal_values()
-            print("\n\n",game.game_log)
-            input("PRESS ENTER")
-            return t
-        
-        possible_actions = list(player.valid_actions) #state.get_possible_actions()
-        print(f"OPTIONS for {player}:", possible_actions)
-
-        action_values = {}
-        expected_value = 0.0
-        if AbstractAction.FOLD in possible_actions and AbstractAction.CHECK in possible_actions:
-                possible_actions.remove(AbstractAction.FOLD)
-
-        for action in possible_actions:
-
-            next_game = deepcopy(game)
-            player_to_act = next_game.get_matching_player(player.ID)
-            print(f"Exploring action: {action} from player {player.name} in round {next_game.game_state}")
-
-            if player_to_act.make_move(next_game, action) == False:
-                continue
-
-            other_player = next_game.get_opponent_player(player_to_act)
-
-            next_state = CFR_State(next_game, next_game.to_act_index)
             
-            action_values[action] = self.cfr_recursive(next_game, other_player)
 
-            
-        #if AbstractAction.BET_HALF in possible_actions and player_to_act.stack - game.pot//2 < 0:
-         #       possible_actions.remove(AbstractAction.BET_HALF)
-        #if AbstractAction.BET_POT in possible_actions and player_to_act.stack - game.pot < 0:
-         #       possible_actions.remove(AbstractAction.BET_POT)
-
-       # if AbstractAction.RAISE_HALF in possible_actions and player_to_act.stack - game.pot//2 < 0:
-        #        possible_actions.remove(AbstractAction.RAISE_HALF)
-        #if AbstractAction.RAISE_POT in possible_actions and player_to_act.stack - game.pot < 0:
-         #       possible_actions.remove(AbstractAction.RAISE_POT)
-
-        #print("OPTIONS_SIMP:", possible_actions)
         
     def train(self):
         player1 = CFR_Player(0, "Player1")
@@ -152,10 +111,86 @@ class CFR_Trainer:
             print(f"Completed {iteration}/10 of training")
             print(f"Current strategy: {player1}")
 
-def cfr(state:CFR_State, p0:float, p1:float) -> float:
-    """counterfactual regret minimisation"""
+def cfr(state:CFR_State, game:Game, p0:float, p1:float) -> float:
+    """
+    counterfactual regret minimisation
+    p0 for button player
+    p1 for bb player
+    """
+    STARTING_STACK = 1000 # sigma
+    SMALL_BLIND = 10
+    BIG_BLIND = 20
 
+    player_to_act = game.players[game.to_act_index]
+    btn_player = game.players[game.dealer_button]
+    bb_player = game.players[(game.dealer_button + 1) % 2]
 
 
     # return payoff for terminal state
+    if state.is_terminal():
+        stack_dict = game.get_terminal_values()
+
+        # net profit relative to starting stack and blinds posted
+        btn_profit = (stack_dict[btn_player] - STARTING_STACK - SMALL_BLIND)
+        bb_profit = (stack_dict[bb_player] - STARTING_STACK - BIG_BLIND)
+
+        return btn_profit if btn_player == player_to_act else bb_profit
+
+    # lookup/create info set 
+    curr_info_set = InformationSet
+
+    # for each action call cfr with additional history and prob
+    strategy = curr_info_set.get_strategy(p0 if player_to_act == btn_player else p1)
+    util = [0.0] * AbstractActions.NUM_ACTIONS
+    node_util = 0
+
+    possible_actions = state.get_possible_actions()
+
+    action_values = {}
+    expected_value = 0.0
+    a = 0
+    for action in possible_actions:
+        next_game = deepcopy(game)
+        next_state = CFR_State(next_game, state.player_id)
+
+        if action == AbstractAction.FOLD:
+            next_game.fold(player_to_act)
+        elif action == AbstractAction.CHECK:
+            next_game.check(player_to_act)
+        elif action == AbstractAction.CALL:
+            next_game.call(player_to_act)
+        elif action == AbstractAction.BET_HALF:
+            next_game.bet(player_to_act, (state.get_pot()//2))
+        elif action == AbstractAction.BET_POT:
+            next_game.bet(player_to_act, state.get_pot())
+        elif action == AbstractAction.RAISE_HALF:
+            next_game.raise_bet(player_to_act, (state.get_pot()//2))
+        elif action == AbstractAction.RAISE_POT:
+            next_game.raise_bet(player_to_act, state.get_pot())   
+        elif action == AbstractAction.ALL_IN:
+            next_game.all_in(player_to_act)
+
+        #time.sleep(0.2)
+        next_player_id = (state.player_id + 1) % len(next_game.players)#
+        next_game.to_act_index = next_player_id
+        next_state = CFR_State(next_game, next_player_id)
+
+        if btn_player == player_to_act:
+            util[a] =  cfr(next_state, next_game, p0 * strategy[a], p1)
+        else:
+            util[a] =  cfr(next_state, next_game, p0, p1 * strategy[a])
+
+        nodeUtil += strategy[a] * util[a];
+        a += 1
+
+    # for each action, compute counterfactual regret
+    a = 0
+    for action in possible_actions:
+        regret = util[a] - node_util
+        curr_info_set.
+
+
+        a += 1
+
+
 CFR_Trainer(1)
